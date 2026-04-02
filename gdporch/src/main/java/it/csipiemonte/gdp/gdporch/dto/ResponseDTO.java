a09caeada5eed19cabe8399a222003dd46d709e1
Pietro-Auletta <pietro.auletta@live.it>
2026-04-02 17:54:06 +0200

    Create ResponseDTO.java

---
+++ gdporch/src/main/java/it/csipiemonte/gdp/gdporch/dto/ResponseDTO.java
@@ -0,0 +1,27 @@
+package it.csipiemonte.gdp.gdporch.dto;
+
+import it.csipiemonte.gdp.gdporch.model.entity.GdpDataUscita;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResponseDTO {
+    public List<GdpDataUscita> uscite = new ArrayList<>();
+    public  List<String> errori = new ArrayList<>();
+
+    public List<GdpDataUscita> getUscite() {
+        return uscite;
+    }
+
+    public void setUscite(List<GdpDataUscita> uscite) {
+        this.uscite = uscite;
+    }
+
+    public List<String> getErrori() {
+        return errori;
+    }
+
+    public void setErrori(List<String> errori) {
+        this.errori = errori;
+    }
+}
