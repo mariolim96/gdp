6534523ed583b25c44c3dae394709d0cbe45ad7f
Giulio-Marra <160647593+Giulio-Marra@users.noreply.github.com>
2026-04-02 17:23:45 +0200

    Aggiunto metodo nella repository

--- gdporch/src/main/java/it/csipiemonte/gdp/gdporch/model/repository/GdpTestataRepository.java
+++ gdporch/src/main/java/it/csipiemonte/gdp/gdporch/model/repository/GdpTestataRepository.java
@@ -20,4 +20,7 @@ public class GdpTestataRepository implements PanacheRepositoryBase<GdpTestata, I
     public List<GdpTestata> findByProvincia(String provincia) {
         return list("provincia", provincia);
     }
+    public List<GdpTestata> findByCartella(String cartellaTestata) {
+        return list("cartellaTestata", cartellaTestata);
+    }
 }
