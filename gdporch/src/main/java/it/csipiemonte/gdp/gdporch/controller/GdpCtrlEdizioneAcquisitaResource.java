73624fa2dbdbb3dad007ca6f878af5362a905951
Pietro-Auletta <pietro.auletta@live.it>
2026-04-10 11:52:50 +0200

    refactor(controller): recupero dinamico path per edizioni acquisite
    Implementata la logica di recupero del path_edizione tramite
    id_gdp_log e id_gdp_edizione per mantenere la compatibilità
    con la nuova firma di DamTrasmissioneService nelle operazioni manuali.

--- gdporch/src/main/java/it/csipiemonte/gdp/gdporch/controller/GdpCtrlEdizioneAcquisitaResource.java
+++ gdporch/src/main/java/it/csipiemonte/gdp/gdporch/controller/GdpCtrlEdizioneAcquisitaResource.java
@@ -4,27 +4,33 @@ import it.csipiemonte.gdp.gdporch.api.ApiApi;
 import it.csipiemonte.gdp.gdporch.dto.EdizioneInsertResponse;
 import it.csipiemonte.gdp.gdporch.dto.GenericProcessResponse;
 import it.csipiemonte.gdp.gdporch.dto.XmlCreationResponse;
+import it.csipiemonte.gdp.gdporch.model.entity.GdpEdizione;
+import it.csipiemonte.gdp.gdporch.model.entity.GdpLogEdizione;
+import it.csipiemonte.gdp.gdporch.model.repository.GdpLogEdizioneRepository;
 import it.csipiemonte.gdp.gdporch.service.DamTrasmissioneService;
 import it.csipiemonte.gdp.gdporch.service.GdpCtrlEdizioneAcquisitaService;
 import it.csipiemonte.gdp.gdporch.service.GdpEdizioneService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class GdpCtrlEdizioneAcquisitaResource implements ApiApi {
 
     private final GdpCtrlEdizioneAcquisitaService ctrlEdizioneAcquisitaService;
     private final GdpEdizioneService edizioneService;
     private final DamTrasmissioneService trasmissionService;
-
+    private final GdpLogEdizioneRepository  gdpLogEdizioneRepository;
     @Inject
     public GdpCtrlEdizioneAcquisitaResource(
             GdpCtrlEdizioneAcquisitaService ctrlEdizioneAcquisitaService,
             GdpEdizioneService edizioneService,
-            DamTrasmissioneService trasmissionService) {
+            DamTrasmissioneService trasmissionService,
+            GdpLogEdizioneRepository gdpLogEdizioneRepository) {
         this.ctrlEdizioneAcquisitaService = ctrlEdizioneAcquisitaService;
         this.edizioneService = edizioneService;
         this.trasmissionService = trasmissionService;
+        this.gdpLogEdizioneRepository = gdpLogEdizioneRepository;
     }
 
     @Override
@@ -45,7 +51,10 @@ public class GdpCtrlEdizioneAcquisitaResource implements ApiApi {
     @Override
     public Response orchInternalCreaXML(Integer idLog, Integer priorita, Integer idTestata,
             Integer idEdizione) {
-        XmlCreationResponse result = trasmissionService.creaXMLEdizione(idTestata, idLog, idEdizione, priorita);
+        Optional<GdpLogEdizione> logEd = gdpLogEdizioneRepository.find("fkGdpLog = ?1 and fkGdpEdizione = ?2",idLog,idEdizione).stream().findFirst();
+        String pathRecuperato = logEd.map(le -> le.pathEdizione).orElse("");
+        //Chiamata F09 con firma nuova
+        XmlCreationResponse result = trasmissionService.creaXMLEdizione(idTestata, idLog, idEdizione, priorita,pathRecuperato);
         return Response.ok(result).build();
     }
 }
